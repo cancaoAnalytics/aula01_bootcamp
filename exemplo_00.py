293618dc658a1bfb689419441e806873a3884b1c
Cancão Analytics <lejt.data@gmail.com>
2026-03-14 18:46:54 -0300

    feat: adds example 00 file

--- exemplo_00.py
+++ exemplo_00.py
@@ -1,1 +1,7 @@
-print("Hello World")
+nome = input("Digite seu nome: ")
+salario = float(input("Digite seu salário: "))
+bonus_percentage = float(input("Percentual de bônus (de 0.0 a 100.0): "))
+
+bonus = 1000 + (salario * (bonus_percentage/100))
+
+print(f"{nome}, seu bônus é de {bonus:.2f}.")
